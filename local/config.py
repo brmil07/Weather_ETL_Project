17931ba1f0ec5e557ad6e5b267c2729e6de46e8a
brmil07 <bryanmilleanno@gmail.com>
2024-08-12 21:27:00 +0200

    moved the api key to secrets.py

--- local/config.py
+++ local/config.py
@@ -1,4 +1,4 @@
-# My API key
-API_KEY = 'c398a640e680959730b54b96d6876dda'
+from secrets import API_KEY
+
 # Base URL for the OpenWeatherMap API
 BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?'
